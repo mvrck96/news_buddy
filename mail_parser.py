d7dce1bf1475023662245af6bbe1e48978554756
mvrck96 <maverickmsk1@gmail.com>
2021-07-16 21:49:48 +0300

    email module added

---
+++ mail_parser.py
@@ -0,0 +1,7 @@
+import email
+import os
+import imaplib
+
+with open("email_credits.txt", "r") as email_credits:
+    user, pwd = [item.strip() for item in email_credits.readlines()]
+print(user, pwd)
