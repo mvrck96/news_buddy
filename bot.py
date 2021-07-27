77a667599bd618d66c082fdbd3a27b7b87600217
mvrck96 <maverickmsk1@gmail.com>
2021-07-27 23:13:15 +0300

    exception handler added

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 from telebot import TeleBot
 import habr_parser
-from telegram import ParseMode
+from telegram import ParseMode, chat
 
 with open("token.txt", "r") as f:
     TOKEN = f.readline().strip()
 
 bot = TeleBot(TOKEN)
+print("Bot is live !!!")
 
 
 @bot.message_handler(commands=["habr"])
-def start(message):
+def habr_digest(message):
     data = habr_parser.parse_habr(top="/top/daily")
     post = habr_parser.get_md_message(data)
     bot.send_message(
@@ -18,6 +19,20 @@ def start(message):
         text=post,
         parse_mode=ParseMode.MARKDOWN,
         disable_web_page_preview=True,
+    )
+    print("Habr digest shipped !")
+
+
+def not_habr(message: str) -> bool:
+    return True if message == "/habr" else False
+
+
+@bot.message_handler(func=not_habr)
+def base_reply(message):
+    bot.reply_to(
+        chat=message.chat.id,
+        text="Sorry, no supported comands except `/habr`",
+        parse_mode=ParseMode.MARKDOWN,
     )
 
 
