63717a163f145f9ce062b8f3e4d52eaeae8f84e8
mvrck96 <maverickmsk1@gmail.com>
2021-07-28 22:44:12 +0300

    new commands added

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
+from datetime import date
 from telebot import TeleBot
-import habr_parser
 from telegram import ParseMode, chat
+
+import habr_parser
+import gazeta_parser
+import rbc_parser
+
 
 with open("token.txt", "r") as f:
     TOKEN = f.readline().strip()
@@ -10,7 +15,7 @@ print("Bot is live !!!")
 
 
 @bot.message_handler(commands=["habr"])
-def habr_digest(message):
+def habr_digest(message) -> None:
     data = habr_parser.parse_habr(top="/top/daily")
     post = habr_parser.get_md_message(data)
     bot.send_message(
@@ -22,12 +27,38 @@ def habr_digest(message):
     print("Habr digest shipped !")
 
 
+@bot.message_handler(commands=["gazeta"])
+def gazeta_digest(message) -> None:
+    digest = rbc_parser.parse_rbc()
+    post = rbc_parser.get_md_message(digest)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=post,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+    )
+    print("Gazeta.ru digest shipped !")
+
+
+@bot.message_handler(commands=["rbc"])
+def gazeta_digest(message) -> None:
+    digest = gazeta_parser.parse_gazeta()
+    post = gazeta_parser.get_md_message(digest)
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=post,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+    )
+    print("RBC digest shipped !")
+
+
 def not_habr(message: str) -> bool:
-    return False if message == "/habr" else True
+    return True if message != "/habr" else False
 
 
 @bot.message_handler(func=not_habr)
-def base_reply(message):
+def base_reply(message) -> None:
     bot.send_message(
         chat_id=message.chat.id,
         text="Sorry, no supported comands except `/habr`",
