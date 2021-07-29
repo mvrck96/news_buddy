061e51caa43259b354d91420fe47c4d6f843843e
mvrck96 <maverickmsk1@gmail.com>
2021-07-29 23:51:38 +0300

    minor changes in dialog forms

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from datetime import date
 from telebot import TeleBot
-from telegram import ParseMode, chat
+from telegram import ParseMode, chat, parsemode
 
 import habr_parser
 import gazeta_parser
@@ -16,6 +16,7 @@ print("Bot is live !!!")
 
 @bot.message_handler(commands=["habr"])
 def habr_digest(message) -> None:
+    bot.send_message(chat_id=message.chat.id, text="Gathering news for you !")
     data = habr_parser.parse_habr(top="/top/daily")
     post = habr_parser.get_md_message(data)
     bot.send_message(
@@ -54,7 +55,11 @@ def gazeta_digest(message) -> None:
 
 
 def not_habr(message: str) -> bool:
-    return True if message != "/habr" else False
+    return (
+        True
+        if str(message).strip().lower() in ["habr", "start", "help", "rbc", "gazeta"]
+        else False
+    )
 
 
 @bot.message_handler(func=not_habr)
@@ -63,6 +68,16 @@ def base_reply(message) -> None:
         chat_id=message.chat.id,
         text="Sorry, no supported comands except `/habr`",
         parse_mode=ParseMode.MARKDOWN,
+    )
+
+
+@bot.message_handler(commands=["start", "help"])
+def helping_greeting(message) -> None:
+    help_message = """
+    Hi, this is *News buddy*. I can send you news from habr.com, gazeta.ru and rbc.ru\n -  To check top daily posts from selected hubs of habr.com send me `/habr`\n-  If you want top hot news from rbc.ru send `/rbc`\n-  For top news from gazeta.ru type `/gazeta`
+    """
+    bot.send_message(
+        chat_id=message.chat.id, parse_mode=ParseMode.MARKDOWN, text=help_message
     )
 
 
