4140281a5be2915a4fc5fe179aae4a1185b96692
mvrck96 <maverickmsk1@gmail.com>
2021-08-05 22:45:46 +0300

    messed up tests

--- tests/test_habr_parser.py
+++ tests/test_habr_parser.py
@@ -1,20 +1,24 @@
-# import sys
-# sys.path.append("../")
+import sys
 
-from requests import get
+sys.path.append(".")
+
 import bs4 as bs
+from requests import get
 
-import habr_parser as parser
+from news_buddy.habr_parser import get_title_and_link, parse_habr
+
+URL = "https://habr.com/ru/"
 
 
 def test_get_title_and_link():
-    url = "https://habr.com/ru/company/ruvds/blog/top/"
-    page = get(url)
+    link = URL + "company/ruvds/blog/top/"
+    page = get(link)
     soup = bs.BeautifulSoup(page.text, "html.parser")
     to_test = soup.find("div", {"class": "tm-article-snippet"})
-    assert (
-        type(parser.get_title_and_link(to_test)) == tuple
-    ), "Not a tuple returned"
+    assert type(get_title_and_link(to_test)) == tuple, "Not a tuple returned"
 
 
-test_get_title_and_link()
+def test_parse_habr():
+    # Тест не корректный !
+    # Потому что не корректно написана функция. Надо перписывать модули парсинга :(
+    assert parse_habr() == None, "I don't know how to write test !"
