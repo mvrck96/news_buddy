78b8c47121bd9ad4a2f83c347d1c8eeaa0c8b9ec
mvrck96 <maverickmsk1@gmail.com>
2021-08-24 17:13:07 +0300

    test added

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 
 sys.path.append(".")
 
@@ -12,3 +13,7 @@ def test_get_md_message_unified():
     res = f'{name} картина дня от {date.today().strftime("%d.%m.%y")}\n\n\n'
     assert utils.get_md_message_unified(name, []) == res
 
+@pytest.mark.parametrize('test_input, expected', \
+ [('asdasd', True),  ('Habr', True), ('/abrakadabra', True), ('/rbc', False), ('/Gazeta', False)])
+def test_check_invalidity(test_input, expected):
+    assert utils.check_invalidity(test_input) == expected
