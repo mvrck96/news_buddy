be2473e9b1c9f2e4a93c025a2b4ca0df3319dbb7
mvrck96 <maverickmsk1@gmail.com>
2021-09-10 17:29:39 +0300

    test for filtering function added

--- tests/test_habr_parser.py
+++ tests/test_habr_parser.py
@@ -3,8 +3,9 @@ import sys
 sys.path.append(".")
 
 import bs4 as bs
-from news_buddy.habr_parser import get_title_and_link, parse_habr
+from news_buddy import habr_parser 
 from requests import get
+import pytest
 
 URL = "https://habr.com/ru/"
 
@@ -14,10 +15,18 @@ def test_get_title_and_link():
     page = get(link)
     soup = bs.BeautifulSoup(page.text, "html.parser")
     to_test = soup.find("div", {"class": "tm-article-snippet"})
-    assert type(get_title_and_link(to_test)) == tuple, "Not a tuple returned"
+    assert type(habr_parser.get_title_and_link(to_test)) == tuple, "Not a tuple returned"
 
 
 def test_parse_habr():
     # Тест не корректный !
     # Потому что не корректно написана функция. Надо перписывать модули парсинга :(
-    assert parse_habr(top='/top/daily', hubs={}) == {}, "I don't know how to write test !"
+    assert habr_parser.parse_habr(top='/top/daily', hubs={}) == {}, "I don't know how to write test !"
+
+@pytest.mark.parametrize('test_input, expected', [
+    ({1: [(1, 1), (1, 2)], 2: [(1, 2), (2, 6)]}, {1: [(1, 1), (1, 2)], 2: [(2, 6)]}),
+    ({1: [(1, 1), (1, 1), (1, 1)], 2: [(1, 1), (1, 1)]}, {1: [(1, 1)]}), 
+    ({1: [(1, 2), (4, 2), (1, 3)]}, {1: [(1, 2), (4, 2), (1, 3)]}), 
+    ({1: [], 2: [(2, 2)]}, {2: [(2, 2)]})])
+def test_filter_digest(test_input, expected):
+    assert habr_parser.filter_digest(test_input) == expected
