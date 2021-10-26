531404538a78b123b93546367f7be87ef9e40114
mvrck96 <maverickmsk1@gmail.com>
2021-10-26 14:27:37 +0300

    digest message small fix

--- news_buddy/utils.py
+++ news_buddy/utils.py
@@ -10,7 +10,7 @@ COMMANDS = ["/start", "/help", "/habr", "/gazeta", "/rbc"]
 
 def get_md_message_unified(name: str, digest: dict) -> str:
     today = date.today().strftime("%d.%m.%y")
-    message = f"{name} картина дня от {today}\n\n\n"
+    message = f"Новости к {today} от {name}\n\n\n"
     for key in digest:
         tmp = f"--  [{digest[key]}]({key})\n\n"
         message += tmp
