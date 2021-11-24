33c7be091e2afc85f4adb46762f2d01935f69543
mvrck96 <maverickmsk1@gmail.com>
2021-11-24 23:21:29 +0300

    database init script change

--- news_buddy/initdb/init.sql
+++ news_buddy/initdb/init.sql
@@ -1,23 +1,25 @@
 -- Initialisation script for newsbuddy_db
 
-
 CREATE TABLE IF NOT EXISTS news_tass(
   id serial PRIMARY KEY,
-  title VARCHAR,
+  title TEXT,
   link VARCHAR,
-  time TIMESTAMP
+  time TIMESTAMP,
+  source VARCHAR DEFAULT 'tass'
 );
 
 CREATE TABLE IF NOT EXISTS news_rbc(
   id serial PRIMARY KEY,
-  title VARCHAR,
+  title TEXT,
   link VARCHAR,
-  time TIMESTAMP
+  time TIMESTAMP,
+  source VARCHAR DEFAULT 'rbc'
 );
 
 CREATE TABLE IF NOT EXISTS news_gazeta(
   id serial PRIMARY KEY,
-  title VARCHAR,
+  title TEXT,
   link VARCHAR,
-  time TIMESTAMP
+  time TIMESTAMP,
+  source VARCHAR DEFAULT 'gazeta'
 );
