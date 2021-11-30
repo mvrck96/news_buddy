47bc944a56155a2e5204d96c2a3d80e52e481944
mvrck96 <maverickmsk1@gmail.com>
2021-11-30 14:07:38 +0300

    source table added

--- news_buddy/initdb/init.sql
+++ news_buddy/initdb/init.sql
@@ -1,5 +1,4 @@
 -- Initialisation script for newsbuddy_db
-
 CREATE TABLE IF NOT EXISTS news_tass(
   id serial PRIMARY KEY,
   title TEXT,
@@ -23,3 +22,10 @@ CREATE TABLE IF NOT EXISTS news_gazeta(
   time TIMESTAMP,
   source VARCHAR DEFAULT 'gazeta'
 );
+
+CREATE TABLE IF NOT EXISTS sources(source varchar PRIMARY KEY, link VARCHAR);
+
+insert INTO sources(source, link) values ('tass', 'https://tass.ru/');
+insert INTO sources(source, link) values ('rbc', 'https://www.rbc.ru/');
+insert INTO sources(source, link) values ('gazeta', 'https://www.gazeta.ru/');
+insert INTO sources(source, link) values ('habr', 'https://habr.com/');
